aa13d43104ab4727dee318650c7ab94a4cb556e5
antoine-dhersin <147629395+antoine-dhersin@users.noreply.github.com>
2024-05-06 11:36:20 +0200

    Create deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,13 @@
+#Attack Surface Reduction Rules JSON File
+$URL = "https://github.com/OPEN-LAKE-TECHNOLOGY/azure/blob/main/AttackSurfaceReductionRules.json"
+#Convert ASR Rules from JSON
+$ASRRules = (Invoke-WebRequest -Uri $URL -UseBasicParsing).Content | ConvertFrom-Json
+foreach($Rule in $ASRRules){
+
+    $ASRRuleName = $Rule.Name
+    $ASRRuleGUID = $Rule.GUID
+
+    Write-Output -InputObject "Working on $ASRRuleName. Setting the rule to Audit Mode"
+    Add-MpPreference -AttackSurfaceReductionRules_Ids $Rule.GUID -AttackSurfaceReductionRules_Actions AuditMode
+
+}
